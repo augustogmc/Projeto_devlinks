349756e0c180a7736cb6d4e59779302d47c670af
Augusto Gabriel <augustogmc@hotmail.com>
2023-03-24 15:09:16 -0300

    change background based on media queries

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ function toggleMode() {
 
   const img = document.querySelector("#profile img")
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/Mobile/Avatar-light.png")
+    img.setAttribute("src", "./assets/Avatar-light.png")
     img.setAttribute("alt", "Foto de Augusto com o fundo amarelo e com óculos")
   } else {
-    img.setAttribute("src", "./assets/Mobile/Avatar-dark.png")
+    img.setAttribute("src", "./assets/Avatar-dark.png")
     img.setAttribute("alt", "Foto de Augusto com o fundo azul")
   }
 }
